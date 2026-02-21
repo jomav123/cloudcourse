0f38a8412a2abd47b8a85ba1b3b9d7c36920e9fb
jo mav <jo@gmail.com>
2026-02-21 19:37:34 +0100

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from azure.storage.blob import BlobServiceClient
-from sqlalchemy.exc import IntegrityError
 from flask_migrate import Migrate
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
