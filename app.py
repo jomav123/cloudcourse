173e23213bff7b37f4fa8a33665b300f0f46da1b
jo mav <jo@gmail.com>
2026-02-23 12:32:57 +0100

    update app.py, requirements.txt, __pycache__

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+# Load .env file locally
+load_dotenv()
 
 app = Flask(__name__)
 
+# Read connection string from environment variable
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("AZURE_SQL_CONNECTIONSTRING")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+# Example table
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
 @app.route("/")
 def home():
-    return "Hello, Gestime! Your Flask app is running."
+    users = User.query.all()
+    return f"Connected to Azure SQL! Users count: {len(users)}"
 
 if __name__ == "__main__":
     app.run(debug=True)
